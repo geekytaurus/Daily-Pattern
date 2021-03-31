fd846e14f3e1ea363e400ba0f88ed94a908bea0d
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-03-31 10:14:21 +0530

    Create code.c

---
+++ Hollow Rectangle/code.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int row, col, i, j;
+    printf("Enter the number of rows and cols : ");
+    scanf("%d%d", &row, &col);
+
+    for(i = 1; i <= row; i++){
+        for(j = 1; j <= col; j++){
+            if(i==1 || i==row || j==1 || j==col){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
