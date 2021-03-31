87b0930e2b917852c44862b21fcb59ec9789cde0
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-03-31 10:26:45 +0530

    Create code.py

---
+++ Hollow Rectangle/code.py
@@ -0,0 +1,10 @@
+row, col = [int(x) for x in input("Enter the number of rows and cols : ").split()]
+for i in range(row):
+    for j in range(col):
+        if (i==0 or i==row-1 or j==0 or j==col-1):
+            print("*", end='')
+        else:
+            print(" ",end='')
+    print("\n")
+
+
