f966cc886605184814366f1e7034cc9b45089f16
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-04 00:33:40 +0530

    Create code.c

---
+++ 0005. Hollow Inverted Half Pyramid/code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int rows, i, j;
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++){
+        for(j = 1; j <= rows-i+1; j++){
+            if(i == 1 || i == rows || j == 1 || j == rows-i+1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
