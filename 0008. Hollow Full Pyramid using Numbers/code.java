8a6aec909ae78bd71ff6b4f202632760e477e92d
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-06 17:04:48 +0530

    space issues for rows > 10 is solved

--- 0008. Hollow Full Pyramid using Numbers/code.java
+++ 0008. Hollow Full Pyramid using Numbers/code.java
@@ -16,7 +16,12 @@ class hollowFullPyramid_usingNumbers{
           }
           for(int j = 1; j <= i; j++){
               if(j == 1 || i == rows){
-                  System.out.print(j+" ");
+                  if( j <= 9 ){
+                     System.out.print(j+" ");
+                  }
+                  else{
+                      System.out.print(j);
+                  }
               }
               else{
                   System.out.print(" ");
