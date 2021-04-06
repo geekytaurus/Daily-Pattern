c8408a51a773093a34db8debaabee06054f5e3ac
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-06 10:55:50 +0530

    Create code.cpp

---
+++ 0008. Hollow Full Pyramid using Numbers/code.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#define endl "\n"
+using namespace std;
+
+int main(){
+    int rows;
+
+    cout << "Enter the number of rows : ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; i++){
+      for(int j = i; j < rows; j++){
+          cout << " ";
+      }
+      for(int j = 1; j <= i; j++){
+          if(j == 1 || i == rows){
+              cout << j << " ";
+          }
+          else{
+              cout << " ";
+          }
+      }
+      for(int j = 1; j < i; j++){
+          if(j == i-1 && j < rows-1){
+              cout << j+1;
+          }
+          else{
+              cout << " ";
+          }
+      }
+
+
+        cout << endl;
+    }
+
+    return 0;
+}
