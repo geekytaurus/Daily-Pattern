89b20221d81df0c9e96b7ddc24a2da01c35a1947
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-08 19:46:12 +0530

    first commit

---
+++ 0010. full Pyramid using Nums/code.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class fullTri_Nums{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows : ");
+        int rows = input.nextInt();
+        int i, j, k, sp;
+
+        for(i = 1; i <= rows; i++){
+            k = i-1;
+    
+            for(sp = 1; sp <= rows-i; sp++){
+                System.out.print("  ");
+            }
+    
+            for(j = 1; j <= i; j++){
+                if(k >= 10){
+                    System.out.print(++k);
+                }
+                else{
+                    System.out.print(++k +" ");
+                }
+                
+            }
+    
+            for(j = i+1; j <= 2*i-1; j++){
+                if(k >= 10 ){
+                    System.out.print(--k);
+                }
+                else{
+                    System.out.print(--k +" ");
+                }
+                
+            }
+    
+            System.out.print("\n");
+        }
+    
+        System.out.print("\n");
+    }
+}
