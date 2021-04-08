df70b752cd41d9d16299948447167f7faa3e4903
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-08 19:34:24 +0530

    first commit

---
+++ 0010. full Pyramid using Nums/code.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main(){
+    int rows, i, j, sp, k;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &rows);
+
+    for(i = 1; i <= rows; i++){
+        k = i-1;
+
+        for(sp = 1; sp <= rows-i; sp++){
+            printf("  ");
+        }
+
+        for(j = 1; j <= i; j++){
+            if(k >= 10){
+                printf("%d", ++k);
+            }
+            else{
+                printf("%d ", ++k);
+            }
+            
+        }
+
+        for(j = i+1; j <= 2*i-1; j++){
+            if(k >= 10 ){
+                printf("%d", --k);
+            }
+            else{
+                printf("%d ", --k);
+            }
+            
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+   
+   return 0;
+}
+
