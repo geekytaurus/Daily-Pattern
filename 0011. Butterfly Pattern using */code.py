ca27b1b701a915a145c222d3b3abe1037d9b747a
geekytaurus <67371648+geekytaurus@users.noreply.github.com>
2021-04-09 16:37:58 +0530

    first commit

---
+++ 0011. Butterfly Pattern using */code.py
@@ -0,0 +1,20 @@
+rows = int(input("Enter the number of rows : "))
+
+for i in range(1, rows+1):
+    for j in range(1, i+1):
+        print("*", end='')
+    for sp in range(1, (2*rows - 2*i)+1):
+        print(" ", end='')
+    for j in range(1, i+1):
+        print("*", end='')
+    print()
+
+for i in range(rows, 0, -1):
+    for j in range(1, i+1):
+        print("*", end='')
+    for sp in range(1, (2*rows - 2*i)+1):
+        print(" ", end='')
+    for j in range(1, i+1):
+        print("*", end='')
+    print()
+   
