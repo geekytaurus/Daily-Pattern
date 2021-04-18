520622a932ee9842462a805731973cf1a9157c77
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-04-18 20:18:26 +0530

    python code added for "Heart Pattern using Star"

---
+++ 0020. Heart Pattern using Star/code.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the number of Rows : "))
+
+
+for i in range(n//2, n+1, 2):
+    for j in range(1, n-i+1, 2):
+        print(" ", end='')
+    for j in range(1, i+1):
+        print("*", end='')
+    for j in range(1, n-i+1):
+        print(" ", end='')
+    for j in range(1, i+1):
+        print("*", end='')
+    print()
+
+for i in range(n, 0, -1):
+    for j in range(i, n):
+        print(" ", end='')
+    for j in range(1, (i*2-1)+1):
+        print("*", end='')
+    print()
+   
