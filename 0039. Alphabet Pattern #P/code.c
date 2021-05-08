ea11b0194f525c37d34b6acdfbe3c507653f5b90
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-05-08 22:43:51 +0530

    C code adde

---
+++ 0039. Alphabet Pattern #P/code.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    
+    int height;
+
+    printf("\nEnter the Height of P : ");
+    scanf("%d", &height);
+
+    int i, j;
+
+    printf("\n");
+    for (i = 0; i < height; i++) {
+        printf("*");
+        for (j = 0; j < height; j++) {
+            if ((i == 0 || i == height / 2 - 1)
+                && j < height - 1)
+                printf("*");
+            else if (i < height / 2 - 1
+                     && j == height - 1 && i != 0)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+
+    return 0;
+}
