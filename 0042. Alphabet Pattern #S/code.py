7d754db6af1be97874b44da69330a41802627f6e
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-05-11 19:51:02 +0530

    python code added

---
+++ 0042. Alphabet Pattern #S/code.py
@@ -0,0 +1,16 @@
+height = int(input("\nEnter the height of S : "))
+
+for i in range(0, height):
+    for j in range(0, height):
+        if(i == 0 or i == height//2 or i == height - 1):
+            print("*", end='')
+        elif(i < height//2 and j == 0):
+            print("*", end='')
+        elif(i > height//2 and j == height - 1):
+            print("*", end='')
+        else:
+            print(" ", end='')
+    print()
+
+print("\n\n")
+
