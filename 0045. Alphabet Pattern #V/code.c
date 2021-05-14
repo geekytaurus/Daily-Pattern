be383c6dfd6c1685a95e33ab430e52d1cfd1a5b6
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-05-14 23:11:22 +0530

    C code added

---
+++ 0045. Alphabet Pattern #V/code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int height, width;
+
+    printf("\nEnter the Height of V : ");
+    scanf("%d", &height);
+
+    width = (2 * height) - 1;
+
+    
+    printf("\n");
+    for(int i = 1; i <= height; i++){
+        for(int j = 1; j <= width; j++){
+            if(j == i || j == (width-i+1)){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+
+    }
+    printf("\n\n");
+
+    return 0;
+}
