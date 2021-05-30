bae0630bff53c83dab3140a6f1524d70c645ee9e
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-05-30 19:19:16 +0530

    C code added

---
+++ 0057. Numeric Number Pattern #5/code.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main(){
+    int height, width;
+    printf("\nEnter the height of 5 : ");
+    scanf("%d", &height);
+     
+    width = height-2;
+
+    printf("\n");
+    for(int i = 1; i <= height; i++){
+       for(int j = 1; j <= width; j++){
+           if(i == 1){
+               printf("*");
+           }else if((i > 1 && i < height/2+1) && (j == 1)){
+               printf("*");
+           }else if((i == height/2+1 || i == height) && (j != width)){
+               printf("*");
+           }else if((i > height/2+1 && i < height) && (j == width)){
+               printf("*");
+           }else{
+               printf(" ");
+           }
+       }
+       printf("\n");
+   }
+
+    printf("\n\n");
+
+
+    return 0;
+}
