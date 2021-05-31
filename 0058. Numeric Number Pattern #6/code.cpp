85510abb3bce6c0c48353b4d7c29d056f57a7623
geekytaurus115 <67371648+geekytaurus115@users.noreply.github.com>
2021-05-31 19:23:25 +0530

    C++ code added

---
+++ 0058. Numeric Number Pattern #6/code.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int height, width;
+    cout <<"\nEnter the height of 6 : ";
+    cin >>height;
+
+    width = height-2;
+
+    cout <<endl;
+    for(int i = 1; i <= height; i++){
+      for(int j = 1; j <= width; j++){
+          if(i == 1 && (j != 1 && j != width)){
+              cout <<"*";
+          }
+          else if((i > 1 && i < height/2+1) && (j == 1)){
+            cout <<"*";
+          }
+          else if((i == height/2+1) && (j != width)){
+            cout <<"*";
+          }
+          else if((i > height/2+1 && i < height) && (j == 1 || j == width)){
+            cout <<"*";
+          }
+          else if((i == height) && (j != 1 && j != width)){
+            cout <<"*";
+          }
+          else{
+            cout <<" ";
+          }
+      }
+      cout <<endl;
+  
+    }
+
+    cout <<endl <<endl;
+    return 0;
+}
